2b7c1b775705a8ac9a9a17bdf10c27349f89eacf
javier ponce <francisco.ponce@soacmc.com>
2019-09-17 11:29:06 -0500

    limit area for ocr service ended, now it can be used for all documents

--- src/services/limitAreaForOcr.js
+++ src/services/limitAreaForOcr.js
@@ -46,7 +46,7 @@ module.exports.limitAreaForOcrY = limitAreaForOcrY = (infoFromPastOcr, wordLimit
         reject('no word limit for limit the caracters in image');
       }
     });
-    if (direction === 'down') {
+    if (direction === 'up') {
       infoFromPastOcr.forEach(elm => {
         if (elm.boundingPoly.vertices[0].y > ylimit && elm.boundingPoly.vertices[1].y > ylimit) {
           info = info.concat(' ' + elm.description);
@@ -65,26 +65,43 @@ module.exports.limitAreaForOcrY = limitAreaForOcrY = (infoFromPastOcr, wordLimit
   });
 }
 
-// prndiente
 // obtain the characters in a selected area, limit by words or coordenates( (0,0), (Xword,Yword) is the default)
-module.exports.limitAreaForOcrXY = limitAreaForOcrXY = (infoFromPastOcr, wordLimit, wordLimit2 ) => {
+module.exports.limitAreaForOcrXY = limitAreaForOcrXY = (infoFromPastOcr, wordLimit, wordLimit2) => {
   return new Promise((resolve, reject) => {
-    let info = '', xlimit = '', ylimit='', coordX=0, coordY=0;
+    let info = '', xlimit1 = '', ylimit1 = '', xlimit2 = 0, ylimit2 = 0, originX, originY, limitX, limitY;
     infoFromPastOcr.forEach(elm => {
       if (elm.description === wordLimit) {
-        xlimit = elm.boundingPoly.vertices[0].x;
-        ylimit = elm.boundingPoly.vertices[0].y;
+        xlimit1 = elm.boundingPoly.vertices[0].x;
+        ylimit1 = elm.boundingPoly.vertices[0].y;
       }
       if (elm == infoFromPastOcr[infoFromPastOcr.length - 1] && xlimit === '') {
-        reject('no word limit for limit the caracters in image');
+        reject('no word '+wordLimit+'limit for limit the characters in image');
       }
     });
-if(wordLimit2){
 
-}
+    if (wordLimit2 && typeof wordLimit2 === 'string') {
+      infoFromPastOcr.forEach(elm => {
+        if (elm.description === wordLimit2) {
+          xlimit2 = elm.boundingPoly.vertices[0].x;
+          ylimit2 = elm.boundingPoly.vertices[0].y;
+        }
+        if (elm == infoFromPastOcr[infoFromPastOcr.length - 1] && xlimit === '') {
+          reject('no word ' +wordLimit2+ 'limit for limit the characters in image');
+        }
+      });
+    }
+
+    originX = xlimit1 < xlimit2 ? xlimit1 : xlimit2;
+    originY = ylimit1 < ylimit2 ? ylimit1 : ylimit2;
+    limitX = xlimit1 > xlimit2 ? xlimit1 : xlimit2;
+    limitY = ylimit1 > ylimit2 ? ylimit1 : ylimit2;
+
+
 
     infoFromPastOcr.forEach(elm => {
-      if (elm.boundingPoly.vertices[0].x < xlimit && elm.boundingPoly.vertices[1].x < xlimit) {
+      let coordX = elm.boundingPoly.vertices[0].x;
+      let coordY = elm.boundingPoly.vertices[0].y
+      if (coordX >= originX && coordX <= limitX && coordY >= originY && coordY <= limitY) {
         info = info.concat(' ' + elm.description);
       }
     });
